5bb481c4b0d4081cecebf9ec3c8f19de8e45488c
Kaba <kaba8081@gmail.com>
2023-11-16 23:52:40 +0100

    Add autoprefixer, postcss, and tailwindcss dependencies, Edit App.tsx and index.css

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+// this file contains all the constant text on site
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        background: "#131417",
+        text: "#FFFFFF",
+        primary: "#2AE0E9",
+        secondary: "#2C303A",
+        accent: "#E4D944",
+      },
+      fontFamily: ["Poppins, sans-serif"],
+    },
+  },
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  },
+  plugins: [],
+}
+
